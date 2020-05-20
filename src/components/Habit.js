e43c3f795cdcaa7268a8f4eca0838e33ff15a5ee
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2020-05-20 20:11:27 +0100

    add update streak onload

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+const moment = require('moment');
+const today = moment();
 
 class Habit extends Component {
     constructor(props) {
         super(props);
         this.state={
             data: this.props.data,
-            today: false
+            today: false,
+            date: this.props.date
         }
     }
 
@@ -28,6 +31,35 @@ class Habit extends Component {
         return this.setState({today: !this.state.today})
     }
     
+    updateStreak = () => {
+        let entryDate = this.state.data.date_of_entry.split("--")
+        const entry = moment(entryDate[0] + '-' + entryDate[1] + '-' + entryDate[2]);
+        const now = today.format('YYYY-MM-DD');
+        let streak = 0;
+
+        if(this.state.data.frequency === "daily"){
+            if(entry.diff(now, 'days') === 0){
+                streak = this.state.data.streak;
+            }else if(entry.diff(now, 'days') === 1){
+                streak = this.state.data.streak + 1;
+            }
+        }else if(this.state.data.frequency === "weekly"){
+            if(entry.diff(now, 'days') <= 7){
+                streak = this.state.data.streak;
+            }else if(entry.diff(now, 'days') <= 14){
+                streak = this.state.data.streak + 1;
+            }
+        }else if(this.state.data.frequency === "monthly"){
+            if(entry.diff(now, 'months') === 0){
+                streak = this.state.data.streak;
+            }else if(entry.diff(now, 'months') === 1){
+                streak = this.state.data.streak + 1;
+            }
+        }
+        return streak;
+    }
+
+
     render() {
         return (
             <div>
@@ -36,6 +68,7 @@ class Habit extends Component {
                     X
                 </button>
                 <p>{this.state.data.streak}</p>
+                <p>Done today: {this.state.data.date_of_entry === this.state.date && this.state.data.streak >= 1? "Yes": "No"}</p>
                 <label>
                     <input type="checkbox" value={this.state.today} name="today" onChange={this.doneToday} />
                     Have you done this today?
--- src/components/HabitList.js
+++ src/components/HabitList.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import Habit from './Habit';
+const moment = require('moment');
+const today = moment();
 
 class HabitList extends Component {
     constructor(props) {
         super(props);
         this.state ={
             userID: this.props.userID,
-            habits: []
+            habits: [],
+            date: this.props.date
         }
     }
     
     getData = () => {
-        fetch(`http://localhost:8000/api/habits/users/1`)
+        fetch(`http://localhost:8000/api/habits/users/${this.props.userID}`)
         .then(response => response.json())
         .then(habits => {return this.setState({habits: habits})})
         .catch(err => {
@@ -19,8 +22,57 @@ class HabitList extends Component {
         })
     }
 
+    setStreak = (data) => {
+        let entryDate = data.date_of_entry.split("--")
+        const entry = moment(entryDate[0] + '-' + entryDate[1] + '-' + entryDate[2]);
+        const now = today.format('YYYY-MM-DD');
+        let streak = 0;
+
+        if(data.frequency === "daily"){
+            if(entry.diff(now, 'days') <= 1){
+                streak = data.streak;
+            }
+        }else if(data.frequency === "weekly"){
+            if(entry.diff(now, 'days') <= 8){
+                streak = data.streak;
+            }
+        }else if(data.frequency === "monthly"){
+            if(entry.diff(now, 'months') <= 1){
+                streak = data.streak;
+            }
+        }
+        return streak;
+    }
+
+    updateStreak = (habit) => {
+        const updatedData = {
+            streak: this.setStreak(habit)
+        };
+        fetch(`http://localhost:8000/api/habits/${habit.habitID}`, {
+                method: 'PUT', 
+                mode: 'cors', 
+                headers: {
+                  'Content-Type': 'application/json'
+                  // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: JSON.stringify(updatedData) // 
+        })
+    }
+
+    onLoad = async () => {
+        fetch(`http://localhost:8000/api/habits/users/${this.props.userID}`)
+        .then(response => response.json())
+        .then(habits => {habits.map(habit =>{
+               this.updateStreak(habit)
+            })})
+        .then(this.getData())
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
     componentDidMount(){
-        this.getData()
+        this.onLoad()
     }
 
     render() {
@@ -30,7 +82,7 @@ class HabitList extends Component {
                     {this.state.habits.map(habit => {
                         return (
                             <div key={habit.habitID}>
-                                <Habit  data={habit}/>
+                                <Habit  data={habit} date={this.props.date}/>
                             </div>
                         )
                     })}
